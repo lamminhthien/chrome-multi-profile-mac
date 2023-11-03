1ddc9388b4a7a43ac23a5eb0604ca2ff8f383df8
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-11-04 00:32:09 +0700

    Add go to initial url ready

--- chrome-multi.sh
+++ chrome-multi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-# Set the path to the Chrome executable (change if needed)
+# For macos
+# Install chrome-cli
 brew install chrome-cli
 # Set the maximum number of Chrome profiles
 MAX_PROFILES=100
@@ -8,9 +8,13 @@ MAX_PROFILES=100
 # Set the path to the Chrome executable (change if needed)
 CHROME_PATH="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
+# Set the initial URL for the profiles
+INITIAL_URL="https://example.com"
+
 # Create Chrome profiles based on the MAX_PROFILES variable
 for ((i = 1; i <= MAX_PROFILES; i++)); do
     profile_name="Profile$i"
-    "$CHROME_PATH" --user-data-dir="/tmp/$profile_name" --no-first-run --no-default-browser-check &
+    "$CHROME_PATH" --user-data-dir="/tmp/$profile_name" --no-first-run --no-default-browser-check "$INITIAL_URL" &
     sleep 2  # Add a delay to avoid opening all profiles at once
 done
+
