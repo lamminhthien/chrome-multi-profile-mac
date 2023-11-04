296939cfe16380dbca392425159e2d9ac39f682d
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-11-04 08:39:06 +0700

    Add range profile

--- chrome-multi-profile-mac.sh
+++ chrome-multi-profile-mac.sh
@@ -3,7 +3,8 @@
 # Install chrome-cli
 brew install chrome-cli
 # Set the maximum number of Chrome profiles
-MAX_PROFILES=3
+FROM_PROFILES=1
+TO_PROFILES=9
 
 # Set the path to the Chrome executable (change if needed)
 CHROME_PATH="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
@@ -12,7 +13,7 @@ CHROME_PATH="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 INITIAL_URL="https://www.quizne.com/room/h1ebn/multiple-play"
 
 # Create Chrome profiles based on the MAX_PROFILES variable
-for ((i = 1; i <= MAX_PROFILES; i++)); do
+for ((i = FROM_PROFILES; i <= TO_PROFILES; i++)); do
     profile_name="Profile$i"
     "$CHROME_PATH" --user-data-dir="/tmp/$profile_name" --no-first-run --no-default-browser-check "$INITIAL_URL" &
     sleep 2  # Add a delay to avoid opening all profiles at once
--- chrome-multi-profile-window.cmd
+++ chrome-multi-profile-window.cmd
@@ -1,6 +1,7 @@
 @echo off
 REM Set the maximum number of Chrome profiles
-set MAX_PROFILES=10
+set FROM_PROFILES=1
+set TO_PROFILES=9
 
 REM Set the path to the Chrome executable (change if needed)
 set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
@@ -9,8 +10,8 @@ REM Set the initial URL for the profiles
 set INITIAL_URL=https://www.quizne.com/room/h1ebn/multiple-play
 
 REM Create Chrome profiles based on the MAX_PROFILES variable
-for /l %%i in (1, 1, %MAX_PROFILES%) do (
+for /L %%i in (%FROM_PROFILES%, 1, %TO_PROFILES%) do (
     set profile_name=Profile%%i
     start "" %CHROME_PATH% --user-data-dir="C:\Temp\%profile_name%" --no-first-run --no-default-browser-check %INITIAL_URL%
-    timeout /t 2 /nobreak  >nul
+    timeout /t 2 >nul  REM Add a delay to avoid opening all profiles at once
 )
